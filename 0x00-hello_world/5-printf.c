c30b7d6e8a64531c89b98f9f797691e925f7961c
BreAhmazin <bre.cannon.bc@gmail.com>
2023-01-27 08:50:01 -0800

    push

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * Description: main - Prints out a string to stdout.
- * Return: 0 if success.
+ * Return: Always 0 (Sucess)
  */
 int main(void)
 {
-	printf("with proper grammer, but the out is a piece of art,\n");
+	printf("with proper grammer, but the outcome is a piece of art,\n");
 	return (0);
 }
