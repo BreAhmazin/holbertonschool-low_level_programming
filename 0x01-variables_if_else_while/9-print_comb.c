1424d053431c4940d6d57090c1c2d4524f408c72
BreAhmazin <bre.cannon.bc@gmail.com>
2023-01-30 11:07:51 -0800

    push

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+/**
+ * main - Entry Point
+ * Return Always 0
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+int c;
+for (c = 0; c <= 9; c++)
+{
+putchar('0' + c);
+if (c != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
