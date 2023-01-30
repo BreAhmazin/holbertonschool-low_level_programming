5ae92bbcbe31daf6be672978b9c893008301edd9
BreAhmazin <bre.cannon.bc@gmail.com>
2023-01-30 11:23:09 -0800

    push

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+/**
+ *main - Entry point
+ *Return: Always 0
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	char c;
+for (c = 'z'; c >= 'a'; c--)
+{
+	putchar(c);
+}
+	putchar('\n');
+	return (0);
+}
