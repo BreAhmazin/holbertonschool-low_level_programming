2fbc6c510390e25a36047d47a24a74fa3776aac5
BreAhmazin <bre.cannon.bc@gmail.com>
2023-01-30 18:40:12 -0800

    push

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+/**
+ *print_alphabet - alphabet
+ *Return: void
+ */
+
+#include "main.h"
+
+void print_alphabet(void)
+
+{
+	char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+	_putchar(c);
+}
+	_putchar('\n');
+}
