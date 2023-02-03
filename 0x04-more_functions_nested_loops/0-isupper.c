0bed63eda2b24da6f2a117c3f7886b884ae00921
BreAhmazin <bre.cannon.bc@gmail.com>
2023-02-03 09:57:56 -0800

    This is more functions and more nested functions

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,18 +3,12 @@
 /**
  * _isupper - Is upper
  * Return : 1 or 0
+ * @c: is a letter
  */
-
 int _isupper(int c)
-
 {
-	char c;
-
-	c = 'A';
-putchar("Return value when uppercase character %c is passed to isupper(): %d", c, isupper(c));
-
-	c = 'a';
-putchar("\nReturn value when another character %c is passed to isupper(): %d", c, isupper(c));
-
-return 0;
-}	
+if (c >= 'A' && c <= 'Z')
+	return (1);
+else
+	return (0);
+}
--- 0x04-more_functions_nested_loops/0-main.c
+++ 0x04-more_functions_nested_loops/0-main.c
@@ -3,13 +3,11 @@
  * main Entry Point
  * Return Always 0
  */
-
-int main(void
-
+int main(void)
 {
 	print_diagonal(0);
 	print_diagonal(2);
 	print_diagonal(10);
 	print_diagonal(-4);
-	return 0;
+	return (0);
 }
