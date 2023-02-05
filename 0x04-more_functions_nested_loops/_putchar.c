4827a4b147b5dea2b09004d0958f0b2097d995b6
BreAhmazin <bre.cannon.bc@gmail.com>
2023-02-05 07:56:36 -0800

    push

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,10 @@
+/**
+ *_putchar - writes the character c pto stdout                      
+ */                                                                 
+#include <unisted.h>                                                
+#include <stdio.h>                                                  
+                                                                     
+ int _putchar(char c)                                                
+{                                                                   
+	        return (write(1, &c, 1));                                   
+}             
