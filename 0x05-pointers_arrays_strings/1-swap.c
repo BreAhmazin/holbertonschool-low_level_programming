5578be99a80c7ec861fc43abaad76ac0b1d98c19
BreAhmazin <bre.cannon.bc@gmail.com>
2023-02-06 10:05:09 -0800

    "1-swap.c"

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+/**
+ * swap_int - Swaps the value of two integers
+ * @a: First integer
+ * @b: Second integer
+ * Return: None
+ */
+void swap_int(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
