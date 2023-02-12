443a35bcbfdcec8fd8fc329cd4b2d604811e2d80
BreAhmazin <bre.cannon.bc@gmail.com>
2023-02-11 18:27:49 -0800

    leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+/**
+ * leet - encodes string to 1337?
+ * @str: the string being changed/encoded
+ * Return: a pointer to encoded sting
+ */
+#include "main.h"
+char *leet(char *str)
+{
+	int index = 0, index2;
+	char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
+
+	while (str[index])
+	{
+		for (index2 = 0; index2 <= 7; index2++)
+		{
+			if (str[index] == leet[index2] ||
+			str[index] - 32 == leet[index2])
+				str[index] = index2 + '0';
+		}
+
+		index++;
+	}
+	return (str);
+}
