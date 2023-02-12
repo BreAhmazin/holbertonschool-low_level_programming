7b05441bdf129295265f428ad23ae46e9879b872
BreAhmazin <bre.cannon.bc@gmail.com>
2023-02-11 17:35:48 -0800

    string_toupper

--- 0x06-pointers_arrays_strings/main.c
+++ 0x06-pointers_arrays_strings/main.c
@@ -1,27 +1,5 @@
-/**
- * print_array - print an array of integers
- * @a: an array of intergers
- * @n: the number of elements tp swap
- * Return: nothing
- */
 #include "main.h"
 #include <stdio.h>
-void print_array(int *a, int n)
-{
-	int i;
-
-	i = 0;
-	while (i < n)
-	{
-	if (i != 0)
-	{
-		printf(", ");
-	}
-	printf("%d", a[i]);
-	i++;
-	}
-printf("\n");
-}
 
 /**
  * main - check the code for Holberton School students.
@@ -30,10 +8,11 @@ printf("\n");
  */
 int main(void)
 {
-	int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 98, 1024, 1337};
+	char str[] = "Holberton School!\n";
+	char *ptr;
 
-	print_array(a, sizeof(a) / sizeof(int));
-	reverse_array(a, sizeof(a) / sizeof(int));
-	print_array(a, sizeof(a) / sizeof(int));
+	ptr = string_toupper(str);
+	printf("%s", ptr);
+	printf("%s", str);
 	return (0);
 }
